60566358b94ab548b0070d03e22a8171d0ecebe2
Abhishek Pavithran <abhishekpavi6@gmail.com>
2025-04-19 09:39:50 +0530

    finsih

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 require('./lib/db')
 const cors = require('cors')
 const cookieparser = require('cookie-parser')
-const path = require('path')
 
 const authRoutes = require('./routes/authRoute')
 const messageRoutes = require('./routes/messageRoute')
@@ -15,7 +14,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const PORT = process.env.PORT
-const __dirname = path.resolve();
+
 
 app.use(cookieparser())
 app.use(
@@ -28,13 +27,7 @@ app.use(
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    }
-    );
-}
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
